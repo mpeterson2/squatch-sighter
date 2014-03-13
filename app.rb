f4241249d7076ee06c670454aa56ef2828718ded
Michael Peterson <mpeterson418@gmail.com>
2014-03-13 16:32:44 -0500

    Removed old files.

--- app.rb
+++ app.rb
@@ -3,10 +3,4 @@
 require_relative "./squatch-sighter"
 require_relative "./database"
 
-
-# configuring for dart.
-#SquatchSighter.set :public_folder, 'public/squatch_sighter/web'
-#SquatchSighter.mime_type :dart, "application/dart"
-#SquatchSighter.set :public_folder, 'public/squatch_sighter/build/web'
-
 SquatchSighter.run!
