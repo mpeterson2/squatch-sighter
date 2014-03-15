f87531a659e5207a274cf708127fc98d5875e65c
Michael Peterson <mpeterson418@gmail.com>
2014-03-15 03:26:53 -0500

    No more command line args.

--- app.rb
+++ app.rb
@@ -1,30 +1,6 @@
 #!/usr/bin/env ruby
 
-require "optparse"
-require "pp"
-
-options = {:dart => false}
-
-optparse = OptionParser.new do|opts|
-  opts.on( '-h', '--help', 'Display this screen' ) do
-    puts opts
-    exit
-  end
-
-  opts.on("-d", "--dart", "Serve Dart files") do
-  	options[:dart] = true
-  end
-
-end.parse!
-
-pp "Options:", options
-pp "ARGV:", ARGV
-
 require_relative "./squatch-sighter"
 require_relative "./database"
 
-if options[:dart]
-	SquatchSighterDart.run!
-else
-	SquatchSighter.run!
-end
+SquatchSighter.run!
