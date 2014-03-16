1ad2dfd4c4c1482166e9b3cdede8ac0ab3effd4c
Michael Peterson <mpeterson418@gmail.com>
2014-03-16 18:08:34 -0500

    No more deleting dropbox files when database.db doesn't exist.

--- app.rb
+++ app.rb
@@ -1,12 +1,5 @@
 #!/usr/bin/env ruby
 
-# Delete the dropbox files if we are starting from a fresh database.
-require_relative "./dropbox"
-
-if not File.exists?("database.db")
-	DropboxApi.new.clear
-end
-
 # Create the database and include the Sinatra application.
 require_relative "./squatch-sighter"
 
